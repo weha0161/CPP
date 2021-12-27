e2345878bbefd449300a435405f0eca294d97f87
markus <markus@gmx.de>
2021-12-27 14:32:25 +0100

    Qunatrity qith unItPrefix

---
+++ Unit/SIPrefix.hpp
@@ -0,0 +1,25 @@
+#include <memory>
+#include <ratio>
+#include "../Fraction/Fraction.h"
+
+#ifndef SIPREFIX_HPP
+#define SIPREFIX_HPP
+
+template<int Ex = 0>
+struct SIPrefix
+{
+// 	using Type = Fraction<N,D>::Type;
+	static const std::string Sign;
+	static constexpr double Factor = 1;
+};
+
+// template<> std::string SIPrefix<1000,1>::Name = "Kilo"
+// using Kilo = SIPrefix<1000,1>::Type;
+
+struct Kilo: public SIPrefix<3>
+{
+    inline static const std::string Sign = "Kilo";
+};
+
+
+#endif
