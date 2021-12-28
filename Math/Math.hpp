857cc096c5025a04ebd83702c56090ec866223c2
markus <markus@gmx.de>
2021-12-28 16:26:00 +0100

    NegatieExponent

--- Math/Math.hpp
+++ Math/Math.hpp
@@ -1,8 +1,6 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
-#include <fstream>
-#include <chrono>
 #include <ctime>
 #include <iterator>
 #include <vector>
@@ -10,7 +8,6 @@
 #include <filesystem>
 #include "../Logger/Logger.hpp"
 #include "../Wrapper/Wrapper.hpp"
-// #include "../Typelist/Typelist.h"	
 
 #ifndef MATH_HPP
 #define MATH_HPP
@@ -18,16 +15,42 @@
 //-------------------------------------------------------------Get by Num-----------------------------------------------------------
 
 template<int N, int Ex>
-struct Pow
+struct Calculate
 {
-	static int constexpr Result = N * Pow<N,Ex-1>::Result;
+	static double constexpr Result = N * Calculate<N,Ex-1>::Result;
 };
 
 template<int N>
-struct Pow<N,0>
+struct Calculate<N,0>
 {
-	static int constexpr Result = 1;
+	static double constexpr Result = 1;
 };
+
+
+template <int N, int Ex, bool isNegative = false> 
+struct GetPow 
+{
+	static double constexpr Result = Calculate<N,Ex>::Result;
+};
+
+template <int N, int Ex> 
+struct GetPow<N, Ex,true> 
+{
+	static double constexpr Result = 1 / (Calculate<N,-Ex>::Result);
+};
+
+template <int N, int Ex> 
+struct Pow 
+{
+	static double constexpr Result = GetPow<N,Ex, Ex < 0>::Result;
+};
+
+// template<int N>
+// struct Pow<N,0,false>
+// {
+// 	static double constexpr Result = 1;
+// };
+
 
 //-------------------------------------------------------
 
--- Math/MathTest.cpp
+++ Math/MathTest.cpp
@@ -5,9 +5,18 @@ int main()
 {       
     std::cout<<"START"<<std::endl;
 	
+    std::cout<<"\n>0"<<std::endl;
 	std::cout<<Pow<10,1>::Result<< std::endl;
 	std::cout<<Pow<10,2>::Result<< std::endl;
 	std::cout<<Pow<10,3>::Result<< std::endl;
+    
+    std::cout<<"\n=0"<<std::endl;
+	std::cout<<Pow<10,0>::Result<< std::endl;
+	
+    std::cout<<"\n<0"<<std::endl;
+	std::cout<<Pow<10,-1>::Result<< std::endl;
+	std::cout<<Pow<10,-2>::Result<< std::endl;
+	std::cout<<Pow<10,-3>::Result<< std::endl;
     
 	std::cout<<"END"<< std::endl;
 
