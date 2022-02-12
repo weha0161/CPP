2d0c73732b13ca026c60201dbddede580d2a0ce8
markus <markus@gmx.de>
2022-02-12 14:40:21 +0100

    AccountTransfer_hpp

--- File/Transfer.hpp
+++ File/Transfer.hpp
@@ -15,12 +15,14 @@
 #include <boost/mpl/placeholders.hpp>
 #include <filesystem>
 #include "TransactionContainer.hpp"
+#include "AccountTransfer.hpp"
 #include "../Logger/Logger.hpp"
 #include "../CSV/CSV.hpp"
 #include "../Quantity/Quantity.h"
 #include "../Typelist/Typelist.h"
 #include "../Visitor/Visitor.hpp"
 #include "../String/String_.hpp"
+
 
 #ifndef TRANSFER_HPP
 #define TRANSFER_HPP
@@ -29,64 +31,6 @@ namespace fs = std::filesystem;
 
 namespace Bank
 {
-	template<typename Direction>
-	struct Transfer
-	{
-		using Type = Transfer<Direction>;
-		using ValueType = Direction;
-		inline static const std::string Id = Direction::Id;
-	};
-	
-	struct In
-	{
-		using Type = In;
-		inline static const std::string Id = "In"; 
-	};
-	
-	struct Out
-	{
-		using Type = Out;
-		inline static const std::string Id = "Out"; 
-	};
-	
-	//-----------------------------------------------------------------------------------------------AccountTransfer-----------------------------------------------------------------------
-	
-	template<typename Account, typename Direction>
-	class AccountTransfer
-	{
-		using Type = AccountTransfer<Account,Direction> ;
-		
-		Key owner;
-		Entry transaction;
-		Entry cause;
-		Date date;
-		IBAN iban;
-		BIC bic;
-		Quantity<Sum> value;
-
-	protected:
-		using CSVSeparator = T::char_<';'> ;
-	public:
-		using AccountType = Account ;
-		using DirectionType = Direction ;
-		using KeyType = Key;
-		using QunatityType = Quantity<Sum>;
-		
-		AccountTransfer(std::string k, std::string c, double v, std::string d, std::string i = "IBAN", std::string b = "BIC", std::string cause_ = "") : owner(k), transaction(c), date(d), value(v), iban(i), bic(b), cause(cause_) 
-		{ 
-			Logger::Log()<<this->cause<<std::endl;
-		};
-		
-		const Key& GetOwner() const { return owner; }
-		const Entry& GetTransaction() const { return transaction; }
-		const Entry& GetCause() const { return cause; }
-		const Date& GetDate() const { return date; }
-		const IBAN& GetIBAN() const { return iban; }
-		const BIC& GetBIC() const { return bic; }
-		const Quantity<Sum>& GetQuantity() const { return value; }
-		const Direction& GetDirection() const { return Direction::Id; }		
-	};
-	
 	//-----------------------------------------------------------------------------------------------TranferEndpoint-----------------------------------------------------------------------
 	
 	template<typename Account, typename Direction, template<typename> class Cont = std::vector>
