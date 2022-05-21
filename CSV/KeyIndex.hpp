05ec2ec3c38ee8ece71cd9abe662330283199632
markus <markus@gmx.de>
2022-05-21 15:23:18 +0200

    KeyIndex with ostream

--- CSV/KeyIndex.hpp
+++ CSV/KeyIndex.hpp
@@ -24,15 +24,27 @@ namespace CSV
 	class KeyIndex
 	{
 		public:
+			KeyIndex(TKey k): key{k}{};
 			using KeyType = Key<TKey>;
 			using IndexType = Index<TIndex>;
 			bool Is(std::string k){ return this->key.Matches(k);}
 			void setIndex(TIndex i) { this->index.setValue(i); }
+			
+			std::ostream& Display(std::ostream& os) const
+			{
+				return os<<this->key<<": "<<std::endl;		
+			}
 		
 		private:
 			Key<TKey> key;
 			Index<TIndex> index;
 	};
+	
+	template<typename TKey = std::string, typename TIndex = uint>
+	std::ostream& operator<<(std::ostream& strm, const KeyIndex<TKey,TIndex> c)
+	{
+		return c.Display(strm);
+	}
 	
 	template<typename AccountType,typename TKey = std::string, typename TIndex = uint>
 	class KeyIndexContainer
