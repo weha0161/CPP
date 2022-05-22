ac5068fb2d3608356f36c15458247759baa630a9
markus <markus@gmx.de>
2022-05-22 20:11:32 +0200

    ReafctoringKexIndexAndGetters

--- CSV/KeyIndex.hpp
+++ CSV/KeyIndex.hpp
@@ -21,18 +21,18 @@ namespace CSV
 			ValueType value;
 	};
 	
-	template<typename TKey = std::string, typename TIndex = uint>
+	template<typename TKeyValue = std::string, typename TIndexValue = uint>
 	class KeyIndex
 	{
 		public:
-			KeyIndex(TKey k): key{k}{};
-			using KeyType = Key<TKey>;
-			using IndexType = Index<TIndex>;
-			using KeyIndexType = KeyIndex<TKey,TIndex>;
+			KeyIndex(TKeyValue k): key{k}{};
+			using KeyType = Key<TKeyValue>;
+			using IndexType = Index<TIndexValue>;
+			using KeyIndexType = KeyIndex<TKeyValue,TIndexValue>;
 			using Iterator = typename KeyType::Iterator;
 			bool Is(std::string k){ return this->key.Matches(k);}
-			void setIndex(TIndex i) { this->index.setValue(i); }
-			void setKeyPatterns(Iterator begin, Iterator end) { this->key.UpdatePatterns(begin,end); }
+			void seTIndexValue(TIndexValue i) { this->index.setValue(i); }
+			void seTKeyValuePatterns(Iterator begin, Iterator end) { this->key.UpdatePatterns(begin,end); }
 			
 			std::ostream& Display(std::ostream& os) const
 			{
@@ -40,49 +40,56 @@ namespace CSV
 			}
 			
 			bool operator ==(const KeyType& k){ return this->key == k;  }
+			const KeyType& GetKey() { return this->key; }
+			const IndexType& GetIndex() { return this->index; }
 		
 		private:
-			Key<TKey> key;
-			Index<TIndex> index;
+			KeyType key;
+			IndexType index;
 	};
-	template<typename TKey = std::string, typename TIndex = uint>
-	bool operator ==(const Key<TKey>& k, const KeyIndex<TKey,TIndex>& ki){ return ki == k; }
+	template<typename TKeyValue = std::string, typename TIndexValue = uint>
+	bool operator ==(const Key<TKeyValue>& k, const KeyIndex<TKeyValue,TIndexValue>& ki){ return ki == k; }
 	
-	template<typename TKey = std::string, typename TIndex = uint>
-	bool operator ==(const KeyIndex<TKey,TIndex>& ki, const Key<TKey>& k){ return ki == k; }
+	template<typename TKeyValue = std::string, typename TIndexValue = uint>
+	bool operator ==(const KeyIndex<TKeyValue,TIndexValue>& ki, const Key<TKeyValue>& k){ return ki == k; }
 	
-	template<typename TKey = std::string, typename TIndex = uint>
-	std::ostream& operator<<(std::ostream& strm, const KeyIndex<TKey,TIndex> c)
+	template<typename TKeyValue = std::string, typename TIndexValue = uint>
+	std::ostream& operator<<(std::ostream& strm, const KeyIndex<TKeyValue,TIndexValue> c)
 	{
 		return c.Display(strm);
 	}
 	
-	template<typename AccountType,typename TKey = std::string, typename TIndex = uint>
+	template<typename AccountType,typename TKeyValue = std::string, typename TIndexValue = uint>
 	class KeyIndexContainer
 	{
 		public:
-			using KeyType = Key<TKey>;
-			using IndexType = TIndex;
-			using KeyIndexType = KeyIndex<TKey,TIndex>;
+			using KeyType = Key<TKeyValue>;
+			using IndexType = TIndexValue;
+			using KeyIndexType = KeyIndex<TKeyValue,TIndexValue>;
 			using ContainerType  = std::vector<KeyIndexType>;
 			using ContainerPtrType  = std::unique_ptr<ContainerType>;
-			KeyIndexContainer(ContainerPtrType k): keyIndices{std::move(k)} { Logger::Log()<<AccountType::Name<<": "<<keyIndices->at(0);};
+			KeyIndexContainer(ContainerPtrType k): keyIndices{std::move(k)} { Logger::Log()<<AccountType::Name<<": Key index container created "<<this->keyIndices->size()<<std::endl;};
 			
 			void UpdateKeys(const std::vector<std::string> keys)
 			{
 				for(uint i = 0; i < keys.size(); ++i)
 					for(auto it = this->keyIndices->begin(); it != this->keyIndices->end(); ++it)
 						if(it->Is(keys.at(i)))
-							it->setIndex(i);
+							it->seTIndexValue(i);
 			}
 			
 			void UpdateKeyPatterns(const KeyType&  k, const std::vector<std::string>& patterns)
+			{
+				this->UpdateKeyPatterns(k.Value, patterns);					
+			}
+			
+			void UpdateKeyPatterns(const TKeyValue& k, const std::vector<std::string>& patterns)
 			{
 				try
 				{
 					auto i = std::find(this->keyIndices->begin(),this->keyIndices->end(),k);
 					if(i != this->keyIndices->cend())
-						i->setKeyPatterns(patterns.cbegin(),patterns.cend());
+						i->seTKeyValuePatterns(patterns.cbegin(),patterns.cend());
 					else
 						Logger::Log<Error>()<<"UpdateKeyPatterns Key "<<k<<" not found!"<<std::endl;					
 				}
