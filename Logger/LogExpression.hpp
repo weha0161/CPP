b47824c33839477c0ba1628b1a58e9ad10280432
markus <markus@gmx.de>
2022-06-16 16:51:13 +0200

    LogEXpresion

---
+++ Logger/LogExpression.hpp
@@ -0,0 +1,36 @@
+#ifndef LOGEXPR_HPP
+#define LOGEXPR_HPP
+
+template<typename T = std::string>
+class LogExpression
+{
+	public:
+		using ValueType = T;
+		LogExpression(ValueType v): value{v} {} 
+	
+		const ValueType Value() const { return this->value; }
+	private:
+		const ValueType value;
+};
+
+template<typename E1, typename E2>
+class LogSumExpression
+{
+	public:
+		LogSumExpression(const E1& e1, const E2& e2): e1{e1}, e2{e2} {} 
+	
+		decltype(auto) Value() const { return e1.Value() + e2.Value(); }
+	private:
+		const E1& e1;
+		const E2& e2;
+};
+
+template<typename T>
+inline LogSumExpression<LogExpression<T>,LogExpression<T>> operator<<( const LogExpression<T>& e1, const LogExpression<T>& e2)	{ std::cout<<"LogExp e1 "<<e1.Value()<<" e2 "<<e2.Value()<<std::endl;	return {e1,e2}; }
+
+template<typename E1, typename E2>
+inline LogSumExpression<E1,E2> operator<<( const E1& e1, const E2& e2)	{ std::cout<<"LogSum e1 "<<e1.Value()<<" e2 "<<e2.Value()<<std::endl; return {e1,e2}; }
+
+//~ template<typename E>
+//~ inline std::ostream& operator<<( std::ostream& os, const E& e)	{ return os<<e.Value(); }
+#endif
--- Logger/Test.cpp
+++ Logger/Test.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "Logger.hpp"
+#include "Logger.hpp"
+#include <typeinfo>
+#include <typeindex>
 
 int main()
 {       
     std::cout<<"START"<<std::endl;
 	
 	Logger::Log()<<"Test"<<std::endl;
-	Logger::Log()<<"Test2"<<std::endl;
+	Logger::Log<Error>()<<"Test2"<<std::endl;
+	Logger::Log<Info>()<<"Test2"<<std::endl;
+	
+	LogExpression le1{1};
+	LogExpression le2{2};
+	LogExpression le3{3};
+	std::cout<<le1.Value()<<std::endl;
+	std::cout<<le2.Value()<<std::endl;
+	std::cout<<le3.Value()<<std::endl;
+	
+	auto le = le1<<le2;
+	auto le4 = le<<le3;
+	auto le5 = le4<<le1<<le4<<le1<<le2;
+	
+	std::cout<<typeid(le5).name()<<std::endl;
 	
 	std::cout<<"END"<<std::endl;
 
