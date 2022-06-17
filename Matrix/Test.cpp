77e03132f99b8a0df5ce78e170d650b171f1a24d
markus <markus@gmx.de>
2022-06-17 15:39:30 +0200

    Refactoring

--- Matrix/Test.cpp
+++ Matrix/Test.cpp
@@ -2,6 +2,8 @@
 #include <boost/mpl/vector.hpp>
 #include "Matrix.hpp"
 #include "MatrixOperators.hpp"
+#include <typeinfo>
+#include <typeindex>
 #include "../Traits/Traits.h"
 
 namespace mpl=boost::mpl;
@@ -28,58 +30,27 @@ int main()
 	M m1 = M(2,2);
 	M m3 = M(2,2);
 	M m4 = M(2,2);
-	M m10 = M(2,2);
-	auto m2 = m10;
+	M m2 = M(2,2);
 	m1.InitElements(1);
 	m3.InitElements(2);
 	m4.InitElements(4);
-	m10.InitElements(2);
+	m2.InitElements(2);
 	
-// 	std::cout<<m1<<std::endl;
-// 	std::cout<<m10<<std::endl;
+ 	std::cout<<"M1: \n"<<m1<<std::endl;
+ 	std::cout<<"M2: \n"<<m2<<std::endl;
+ 	std::cout<<"M3: \n"<<m3<<std::endl;
+ 	std::cout<<"M4: \n"<<m4<<std::endl;
 	
-// 	auto m2 = M(5,5);
 	M m5 = M(2,2);
-// 	m5 = 1,2,3,4;
-// 	auto m6 = M(2,2);
-// 	m6 = 1,2,3,4;
-// 	m2.InitElements(4);
-	
-// 	std::cout<<m1<<std::endl;
+	M m6 = (m1 + m3) + m4 + m2;	
 
-// 	auto m6 = (m1 + m3) + m4;	
-	M m6 = (m1 + m3) + m4 + m4;	
-// 	BinaryExpression<AdditionExpression<Matrix<M>, Matrix<M>>> m6 = (m1 + m3);	
-// 	M m6 = (m1 + m3);	
+	std::cout<<"Get m1,2,3,4 +  (1,1): "<<m6.Get(1,1)<<std::endl;
+
+	//~ std::cout<<typeid(m6).name()<<std::endl;
 	
-// 	M m = M(m3);
-// 	M mA = M(2,2);	
-// 	M mB = M(2,2);	
-	
-// 	mA = m3;
-	std::cout<<"ASSIGNMENT"<<std::endl;
-	std::cout<<"Get m3 (1,1): "<<m6.Get(1,1)<<std::endl;
-// 	std::cout<<"Get m=m3 (1,1): "<<m.Get(1,1)<<std::endl;
-// 	mB = mA;
-	
-	auto m9 = m10 * m10;
-// 	std::cout<<"Get m3 (1,1): "<<m9.Get(1,1)<<std::endl;
-	
-// 	std::cout<<m3<<std::endl;
-// 	std::cout<<"M4: \n"<<m4<<std::endl;
-// 	std::cout<<m9<<std::endl;
-// 	std::cout<<"M4 Get: \n"<<m4.Get(1,1)<<std::endl;
-// 	std::cout<<"M9 Get: \n"<<m9.Get(1,1)<<std::endl;
-	
-// 	std::cout<<m5<<std::endl;
-// 	std::cout<<m6<<std::endl;
-	
-// 	auto m7 = m5 + m6;
-// 	auto m8 = m5 + m6 + m7;
-// 	std::cout<<m7<<std::endl;
-// 	std::cout<<m8<<std::endl;
-	
-// 	std::cout<<"Multiplication: \n"<<m9<<std::endl;
+ 	std::cout<<"Multiplication: \n"<<std::endl;
+	auto m9 = m1 * m1;
+	//~ std::cout<<"Get m3 (1,1): "<<m9.Get(1,1)<<std::endl;
 	
 	
 	std::cout<<"END"<<std::endl;
